0c38c5c59da8e2f98c4536a06c2caa631b0abde5
Bruna <brujvieira@yahoo.com.br>
2018-08-15 10:17:02 -0300

    mudanças

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+ var config = {
+    apiKey: "AIzaSyDOCOGVqNAFJpANWosZFxB2KcPIzZ6jwKU",
+    authDomain: "redesocial-e5d37.firebaseapp.com",
+    databaseURL: "https://redesocial-e5d37.firebaseio.com",
+    projectId: "redesocial-e5d37",
+    storageBucket: "redesocial-e5d37.appspot.com",
+    messagingSenderId: "973557043020"
+  };
+
+firebase.initializeApp(config);
